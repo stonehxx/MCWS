26a3cc5c49a7f5ebd244fd98cf34be2c188e2911
stonehxx <123812315+stonehxx@users.noreply.github.com>
2023-02-01 16:40:54 +0800

    Add files via upload

--- START.py
+++ START.py
@@ -1,15 +1,23 @@
+import json
 import asyncio
 import websockets
-import SERVER
+import API
 
 async def echo(websocket):
-    for i in range(SERVER.first_send(0,0)):
-        await websocket.send(SERVER.first_send(1,i))
-    async for message in websocket:
-        print("Client:" + message)
+    try:
+        print("SERVER: CLIENT IN")
+        DATA = ["PlayerMessage"]
+        for i in DATA:
+            print("MODE: " + i + " START SUCCESSFUL")
+            await websocket.send(json.dumps(API.send("subscribe",0,i)))
+        async for message in websocket:
+            await websocket.send(json.dumps(API.reception(json.loads(message))))
+    finally:
+        print("SERVER: CLIENT OUT")
 
 async def main():
-    async with websockets.serve(echo,"192.168.1.11",8080):
+    async with websockets.serve(echo,"192.168.1.10",8080):
         await asyncio.Future()
 
+print("SERVER: SERVER START SUCCESSFUL")
 asyncio.run(main())
